27eefa93dca0b42f35892d8dad9a4ffa0b8b3b50
alexpac27 <alexpac27@gmail.com>
2020-11-30 23:02:10 -0500

    created hooks for state and canvas game board

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
+import {useInterval} from './Container/useInterval'
+import{CANVAS_SIZE, SNAKE_START, APPLE_START, SCALE,SPEED,DIRECTIONS} from './Container/constants'
 import './App.css';
 import GameBoard from './Container/GameBoard'
 import Login from './Container/Login'
 import NavBar from './Container/NavBar'
 
-function App() {
+const App = () => {
+
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0,-1]);
+  const [speed, setSpeed] = useState(null);
+  const [gameOver, setgameOver] = useState(null);
+  
+  const startGame = () => {}
+
+  const endGame = () =>{}
+
+  const moveSnake = () => {}
+
+  const createApple = () => {}
+
+  const checkCollision = () => {
+
+  }
+
+  const checkAppleCollision = () =>{}
+
+  const gameLoop = () => {}
+
+  useEffect(()=>{
+
+  }, [snake, apple, gameOver] )
+
   return (
-    <div className="App">
-      <h1>App Container</h1>
-        <NavBar/>
-        <Login/>
-        <GameBoard/>
+    <div role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
+      <canvas
+        style={{ border: "1px solid black"}}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+        {gameOver && <div>GAME OVER!</div>}  
+        <button onClick={startGame}>Start Game</button>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
